19c119341d42619880ee1ade77f15fd8d906c9d0
Hung Le <hungkhanhle@gmail.com>
2021-11-15 12:16:19 +0700

    Fix bug

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -140,6 +140,8 @@ class CarController():
         else:
           apply_steer = CS.out.steeringAngleDeg
           # Hardcode for testing only
+          if (apply_steer == 360):
+            apply_steer = 0
           apply_steer = self.lastAngle + 20
           self.sappState = 2
           self.angleReq = 1
